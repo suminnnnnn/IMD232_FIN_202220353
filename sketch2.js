4523cc6a4356d112194afcdd8f76a0436e02ea0d
suminnnnnn <126849506+suminnnnnn@users.noreply.github.com@email.com>
2023-12-20 04:07:55 +0900

    기말

--- sketch2.js
+++ sketch2.js
@@ -1,16 +1,18 @@
 let W2, H2;
 let maxCanvasHeight = 600;
-let xspacing = 12;
+let xs = 12;
 let w;
 let theta = 0;
 let amplitude = 75.0;
 let period = 400.0;
 let dx;
-let yvalues;
+let yv;
 let canvas2;
 
 let mouse1;
 let mouse2;
+
+let circles = [];
 
 function setup() {
   let container2 = document.querySelector('#canvas-container-2');
@@ -19,26 +21,32 @@ function setup() {
   canvas2 = createCanvas(W2, H2);
   canvas2.parent(container2);
   w = width + 12;
-  dx = (TWO_PI / period) * xspacing;
-  yvalues = new Array(floor(w / xspacing));
+  dx = (TWO_PI / period) * xs;
+  yv = new Array(floor(w / xs));
+
+  noStroke();
 }
 
 function draw() {
   linearGradient(0, 0, width, height, color(130, 230, 255), color(0, 0, 255));
   Wave();
   whiteWave();
+
   mouse1 = map(mouseX, 0, width, 0, 2);
   mouse2 = map(mouseY, 0, height, 1, 3);
+
+  update();
+  display();
 }
 
 //파도를 표현하기 위해 for 루프를 기반으로 한 코드를 활용하였습니다. 고정된 파형의 기존 코드에 마우스를 활용해 진폭의 넓이, 높이가 바뀌도록 했습니다.
 //https://www.geeksforgeeks.org/how-to-create-animation-of-sine-wave-pattern-using-p5-js/
 function Wave() {
   theta += 0.09;
-  var x = theta;
+  let x = theta;
 
-  for (var i = 0; i < yvalues.length; i++) {
-    yvalues[i] = mouse2 * sin(x * mouse1) * amplitude;
+  for (let i = 0; i < yv.length; i++) {
+    yv[i] = mouse2 * sin(x * mouse1) * amplitude;
     x += dx;
   }
 }
@@ -47,24 +55,68 @@ function whiteWave() {
   noStroke();
   fill(255);
 
-  for (var x = 0; x < yvalues.length; x++) {
+  for (let x = 0; x < yv.length; x++) {
     rect(
-      map(x, 0, yvalues.length, 0, width),
-      map(height / 3 + yvalues[x], 0, height, 0, height),
+      map(x, 0, yv.length, 0, width),
+      map(height / 3 + yv[x], 0, height, 0, height),
       20,
       50
     );
   }
 }
 
+//그라데이션 배경을 적용하고 싶어서 챗지피티의 도움을 빌렸습니다.
 function linearGradient(x, y, w, h, c1, c2) {
   noFill();
 
-  for (var i = y; i <= y + h; i++) {
-    var inter = map(i, y, y + h, 0, 1);
-    var c = lerpColor(c1, c2, inter);
+  for (let i = y; i <= y + h; i++) {
+    let inter = map(i, y, y + h, 0, 1);
+    let c = lerpColor(c1, c2, inter);
     stroke(c);
     line(x, i, x + w, i);
+  }
+}
+
+function display() {
+  for (let i = circles.length - 1; i >= 0; i--) {
+    let circle = circles[i];
+    circle.update();
+    circle.display();
+    if (circle.isOffScreen()) {
+      circles.splice(i, 1);
+    }
+  }
+}
+
+function update() {
+  if (mouseIsPressed) {
+    let circle = new Circle(mouseX, mouseY);
+    circles.push(circle);
+  }
+}
+
+class Circle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = random(5, 20);
+    this.color = color(255, 255, 255, 150);
+    this.speed = random(1, 3);
+    this.aSpeed = random(1, 3);
+  }
+
+  update() {
+    this.y -= this.speed;
+    this.color.setAlpha(this.color.levels[3] - this.aSpeed);
+  }
+
+  display() {
+    fill(this.color);
+    ellipse(this.x, this.y, 2 * this.radius);
+  }
+
+  isOffScreen() {
+    return this.y + this.radius < 0;
   }
 }
 
